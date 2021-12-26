cf088a421ec4b497363821fe39cf1884f3f2524b
Kave Bahraman <sadegh.b218@gmail.com>
2021-12-26 18:44:33 +0330

    added run and setup scripts

---
+++ run.sh
@@ -0,0 +1,1 @@
+python src/train.py --data data/eccv16_dataset_tvsum_google_pool5.h5 --name '5 seed test experiment'
---
+++ setup.sh
@@ -0,0 +1,5 @@
+# download dataset & extract to data folder
+mkdir data -p
+wget https://www.dropbox.com/s/tdknvkpz1jp6iuz/dsnet_datasets.zip
+unzip dsnet_datasets.zip -d data
+rm dsnet_datasets.zip
