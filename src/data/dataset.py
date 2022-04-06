91dad8a6bbb697d87cea5e9ff5ec0a87826040d7
Kave Bahraman <sadegh.b218@gmail.com>
2022-04-06 14:25:20 +0430

    Adds video rep to dataset

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -15,18 +15,22 @@ class PreTrainDataset(Dataset):
         self.data = []
         self.datasets = datasets.split("+")
         for dataset in self.datasets:
-            with h5py.File(os.path.join(root, PATH[dataset]), 'r') as f:
+            dataset_name = PATH[dataset]
+            video_rep_path = os.path.join(root, "video", dataset_name)
+            with h5py.File(os.path.join(root, dataset_name), 'r') as f:
                 for key in f.keys():
                     data = torch.tensor(f[key]['features'][...].astype(np.float32))
-                    self.data.append(data)
+                    video_rep = np.load("%s%s.npy" % (video_rep_path, key))
+                    self.data.append((data, video_rep))
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        features = self.data[idx]
+        features, video_rep = self.data[idx]
+        video_rep = torch.tensor(video_rep)
 
-        return features
+        return features, video_rep
 
 
 def collate_fn_pretrain(batch):
